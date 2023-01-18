d09e8d2a1384ce91af52b88fbb5fd10fd707e589
iqbalrony <iqbalrony30@gmail.com>
2023-01-18 17:47:07 +0600

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,6 @@
 import { createApp } from 'vue'
-// import App from './App.vue'
+import App from './App.vue'
 
-// createApp({
-//   data() {
-//     return {
-// 		return {
-// 			name: 'Admin Page Test',
-// 		}
-//     }
-//   }
-// }).mount('#app');
 const IrWpVueTest = createApp({
 	data(){
 		return {
